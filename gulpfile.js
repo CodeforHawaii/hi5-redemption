a6d6d9c127c2b8f22416f3f4b530e49481455036
George Lee <keoki.lee@gmail.com>
2015-06-07 11:32:59 -1000

    Create initial gulp file and add additional deps.
    Created a new gulp file based on https://github.com/tinkertrain/jspm-react/
    Updated with additional deps.

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,147 @@
 'use strict';
 
 var gulp = require('gulp');
-var less = require('gulp-less');
-var flatten = require('gulp-flatten');
-var jade = require('gulp-jade');
-var livereload = require('gulp-livereload');
-var browserify = require('gulp-browserify');
-var server = require('./server.js');
+var plugins = require('gulp-load-plugins')();
+var rimraf = require('rimraf');
+var browserSync = require('browser-sync');
+var runSequence = require('run-sequence');
+var karma = require('karma');
 
-gulp.task('dev-server', function() {
-  gulp.src('locations.json').pipe(gulp.dest('dist'));
-  server.createServer(3000);
+
+// Start BrowserSync Server
+gulp.task('browser-sync', function() {
+  browserSync.init([
+    './build/css/*.css',
+    './build/js/**/*.js',
+    './**/*.html'
+  ],
+  {
+    notify: false,
+    server: {
+      baseDir: ['./']
+    },
+    port: 3500,
+    browser: [],
+    tunnel: false
+  });
 });
 
-gulp.task('bower', function() {
-  gulp.src('app/bower_components/**/*.min.css')
-      .pipe(flatten())
-      .pipe(gulp.dest('dist/css'));
 
-  gulp.src('app/bower_components/bootstrap-material-design/css-compiled/*')
-      .pipe(flatten())
-      .pipe(gulp.dest('dist/css'));
-
-  gulp.src('app/bower_components/bootstrap/dist/fonts/*')
-      .pipe(gulp.dest('dist/fonts'));
-});
-
-gulp.task('less', function() {
-  gulp.src('app/stylesheets/*.less')
-        .pipe(less({
-            paths: [
-                'app/bower_components/bootstrap/less'
-            ]
-        }))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(livereload());
-});
-
+// Compile JSX
 gulp.task('js', function() {
-  gulp.src('app/javascripts/app.js')
-      .pipe(browserify({
-        transform: ['reactify'],
-        debug: true
-      }))
-      .pipe(gulp.dest('dist/js'))
-      .pipe(livereload());
+  return gulp.src('src/**/*.js')
+    .pipe(plugins.cached('js'))
+    .pipe(gulp.dest('build/'));
 });
 
-gulp.task('templates', function() {
-  gulp.src('app/views/index.jade')
-      .pipe(jade())
-      .pipe(gulp.dest('dist/'))
-      .pipe(livereload());
+
+// Compile Sass
+gulp.task('sass', function() {
+  return gulp.src('src/sass/main.scss')
+    .pipe(plugin.sourcemaps.init())
+    .pipe(plugins.sass())
+    .pipe(plugins.sourcemaps.write({includeConcent: false}))
+    .pipe(plugins.sourcemaps.init({loadMaps: true}))
+    .pipe(plugins.autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .on('error', plugins.util.log)
+    .pipe(plugins.sourcemaps.write('.'))
+    .pipe(gulp.dest('./build/css'))
+    .on('error', plugins.util.log);
 });
 
-gulp.task('watch', function() {
-  gulp.watch('app/stylesheets/**', ['less']);
-  gulp.watch('app/javascripts/**', ['js']);
-  gulp.watch('app/views/**', ['templates']);
+
+// Serve
+gulp.task('serve', ['browser-sync', 'js', 'sass'], function() {
+  plugins.watch('src/sass/**/*.scss', {name: 'SASS'}, function() {
+    gulp.start('sass');
+  });
+
+  plugins.watch('src/js/**/*.js', {name: 'JS'}, function() {
+    gulp.start('js')
+  });
 });
 
-gulp.task('default', ['dev-server', 'js', 'less', 'bower', 'watch', 'templates']);
+
+// Clean
+gulp.task('clean', function() {
+  rimraf('build', function(err) {
+    plugins.util.log(err);
+  });
+});
+
+
+// Build (no server)
+gulp.task('build', ['js', 'sass']);
+
+
+// Default
+gulp.task('default', ['serve']);
+
+
+// Tests
+gulp.task('test', function(done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js'
+  }, done);
+});
+
+
+// Distribution tasks
+//=================================
+
+// Clean Dist folder
+gulp.task('dist-clean', function() {
+  rimraf('./dist', function(err) {
+    plugins.util.log(err);
+  });
+});
+
+
+// CSS
+gulp.task('dist-css', function() {
+  return gulp.src('./build/css/main.css')
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(plugins.csso())
+    .pipe(plugins.rename('main.min.css'))
+    .pipe(gulp.dest('./dist/css'))
+    .on('error', plugins.util.log);
+});
+
+
+// Copy index.html to dist
+gulp.task('html', function() {
+  gulp.src(['./index.html'])
+    .pipe(gulp.dest('./dist'))
+    .on('error', plugins.util.log);
+});
+
+
+// Bundle with jspm
+gulp.task('bundle', ['js'], plugins.shell.task([
+  'jspm bundle-sfx build/js/main.js!jsx dist/js/app.js'
+]));
+
+
+// Uglify bundle
+gulp.task('uglify', function() {
+  return gulp.src('./dist/js/app.js')
+    .pipe(plugins.sourcemaps.init({loadMaps: true}))
+    .pipe(plugins.uglify())
+    .pipe(plugins.sourcemaps.write('.'))
+    .pipe(plugins.rename('app.min.js'))
+    .pipe(gulp.dest('./dist/js'))
+    .on('error', plugins.util.log);
+});
+
+
+//Create distribution
+gulp.task('dist', function() {
+  runSequence(
+    'delete-dist',
+    'build',
+    ['css', 'html', 'bundle']
+    'uglify'
+  );
+});
