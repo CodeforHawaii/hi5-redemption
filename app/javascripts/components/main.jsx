d90ed4e0c21397517ee2545ad65e5b8d919a9d05
George Lee <keoki.lee@gmail.com>
2015-07-14 14:01:40 -1000

    Just some minor refactoring.

--- app/javascripts/components/main.jsx
+++ app/javascripts/components/main.jsx
@@ -36,7 +36,10 @@ var Main = React.createClass({
         <h1>material-ui</h1>
         <h2>example project</h2>
 
-        <RaisedButton label="Super Secret Password" primary={true} onTouchTap={this._handleTouchTap} />
+        <RaisedButton label="Super Secret Password"
+                      primary={true}
+                      onTouchTap={this._handleTouchTap}
+        />
       </div>
     );
   }
